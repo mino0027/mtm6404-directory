4056e47a818441529330f400c5bde1bb10a6ec2d
Chris Minosora <mino0027@algonquinlive.com>
2024-09-16 20:12:37 -0400

    forgot to execute the list function, will do now

--- functions.js
+++ functions.js
@@ -8,3 +8,12 @@
 </li>
 */
 
+/*Create an order function. It will take two arguments (an array of clients, a string of a property in the client object) and return an array of clients sorted by a specified property. The sort() method with an arrow function should be used.*/
+function order(clients, property) {
+    return clients.sort((a, b) => (a[property] > b[property]) ? 1 : ((b[property] > a[property]) ? -1 : 0));
+}
+// this will sort the clients by the property in the function.
+// inside the sort a comparison function is being used, and it decides if the property is greater than the other.
+// the question mark is a ternary operator and it is used to return a value based on a condition. the condition is if a is greater than b.
+// this is followed by if b is less than a and it will give a new value.
+
