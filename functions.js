62b33846c53d5b52f8158eb7e152c8ccbb154096
Chris Minosora <mino0027@algonquinlive.com>
2024-09-16 20:01:25 -0400

    setting instructions into code

---
+++ functions.js
@@ -0,0 +1,10 @@
+/*Create a list function. It will take one argument (an array of clients) and return an HTML string. The string will contain a list item template (shown below) for each client in the array. The map() method with an arrow function should be used.*/
+//<!-- List Item Template -->
+
+/*
+<li class="list-group-item d-flex justify-content-between" data-index="<!-- Replace with client's index -->">
+  <!-- Replace with client's name -->
+  <strong>$ <!-- Replace with client's balance--></strong>
+</li>
+*/
+
